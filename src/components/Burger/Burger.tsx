df52d3c9782d27f479136247c905ea3c1dd95bbb
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-05-30 23:18:14 +0600

    add burger component

---
+++ src/components/Burger/Burger.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+import './Burger.css';
+
+export interface IIngCount {
+  name: string;
+  count: number;
+}
+
+interface IProps {
+  ingredients: IIngCount[];
+}
+
+const Burger: React.FC<IProps> = ({ ingredients }) => {
+  return (
+    <div className="Burger">
+      <div className="BreadTop">
+        <div className="Seeds1"></div>
+        <div className="Seeds2"></div>
+      </div>
+      {
+        ingredients.map((ingredient: IIngCount) => {
+          const allIngredients: React.ReactNode[] = [];
+          for (let i: number = 0; i < ingredient.count; i++) {
+            allIngredients.push(<div key={nanoid()} className={ingredient.name}></div>);
+          }
+          return allIngredients;
+        })
+      }
+      <div className="BreadBottom"></div>
+    </div>
+  );
+};
+
+export default Burger;
